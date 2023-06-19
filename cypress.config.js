06bb023f6a967e57c36367e57f460f714f01b201
Emilia Malecka <emiliamalecka14@gmail.com>
2023-06-19 20:34:26 +0200

    mocha commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '445uuj',
+  "reporter": "mochawesome",
+  "reporterOptions": {
+    "reportFilename": "[status]_[datetime]-[name]-report",
+    "overwrite": false,
+    "html": true,
+    "json": true
+  },
   e2e: {
     viewportHeight: 1080,
     viewportHeight: 1440,
